738c29424f0f55c2329596893b7b7f2793c4ab7b
HARSH <iamhp2k@gmail.com>
2020-11-24 23:52:44 +0530

    ginkgo: CesiumOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_ginkgo.mk
+    $(LOCAL_DIR)/cesium_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_ginkgo-user \
-    arrow_ginkgo-userdebug \
-    arrow_ginkgo-eng
+    cesium_ginkgo-user \
+    cesium_ginkgo-userdebug \
+    cesium_ginkgo-eng
